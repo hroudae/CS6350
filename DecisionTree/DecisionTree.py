43e9619427c262c5b0790d3b61da47080368341a
EVAN HROUDA <u1302032@lab1-10.eng.utah.edu>
2021-02-08 13:51:12 -0700

    add training and test data

---
+++ DecisionTree/DecisionTree.py
@@ -0,0 +1,6 @@
+##########
+# Author: Evan Hrouda
+# Purpose: Implement decision tree learning algorithm using the dataset in the
+#          car directory. Training data is in car/train.csv and test data is in
+#          car/test.csv.
+##########
